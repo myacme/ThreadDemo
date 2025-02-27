1ad94eb9ec726e3ef747e66625f425e4c63b895b
liujx <1030375081@qq.com>
2025-02-27 16:48:07 +0800

    feat(juc): 添加 CountDownLatch 和 Semaphore 的新用法示例- 在 CountDownLatchDemo 中添加了 simultaneousExecution 方法，演示了同时执行多个线程的用法 - 在 SemaphoreDemo 中添加了 alternateExecution 和 executeSequentially 方法，展示了信号量的交替执行和依次执行功能 - 优化了代码格式，增加了注释，提高了代码可读性

--- src/main/java/juc/conditionThread/CountDownLatchDemo.java
+++ src/main/java/juc/conditionThread/CountDownLatchDemo.java
@@ -1,35 +1,67 @@
 package juc.conditionThread;
 
+import java.time.Instant;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 public class CountDownLatchDemo {
     public static void main(String[] args) throws InterruptedException {
 //        general();
-        countDownLatchTest();
+//        countDownLatchTest();
+        simultaneousExecution();
     }
 
-    public static void general(){
+
+    /**
+     * 同时执行
+     */
+    public static void simultaneousExecution() throws InterruptedException {
+        CountDownLatch countDownLatch1 = new CountDownLatch(1);
+        new Thread(() -> {
+            try {
+                countDownLatch1.wait();
+                System.out.println(System.currentTimeMillis());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "A").start();
+        new Thread(() -> {
+            try {
+                countDownLatch1.wait();
+                System.out.println(Instant.now().toEpochMilli());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, "B").start();
+        Thread.sleep(1000);
+        countDownLatch1.countDown();
+    }
+
+    public static void general() {
         for (int i = 1; i <= 6; i++) {
             new Thread(() -> {
-                System.out.println(Thread.currentThread().getName()+"\t上完自习，离开教室");
-            }, "Thread-->"+i).start();
+                System.out.println(Thread.currentThread().getName() + "\t上完自习，离开教室");
+            }, "Thread-->" + i).start();
         }
-        while (Thread.activeCount()>2){
-            try { TimeUnit.SECONDS.sleep(2); } catch (InterruptedException e) { e.printStackTrace(); }
+        while (Thread.activeCount() > 2) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        System.out.println(Thread.currentThread().getName()+"\t=====班长最后关门走人");
+        System.out.println(Thread.currentThread().getName() + "\t=====班长最后关门走人");
     }
 
     public static void countDownLatchTest() throws InterruptedException {
         CountDownLatch countDownLatch = new CountDownLatch(6);
         for (int i = 1; i <= 6; i++) {
             new Thread(() -> {
-                System.out.println(Thread.currentThread().getName()+"\t被灭");
+                System.out.println(Thread.currentThread().getName() + "\t被灭");
                 countDownLatch.countDown();
             }, CountryEnum.forEach_CountryEnum(i).getRetMessage()).start();
         }
         countDownLatch.await();
-        System.out.println(Thread.currentThread().getName()+"\t=====秦统一");
+        System.out.println(Thread.currentThread().getName() + "\t=====秦统一");
     }
 }
--- src/main/java/juc/conditionThread/SemaphoreDemo.java
+++ src/main/java/juc/conditionThread/SemaphoreDemo.java
@@ -4,7 +4,108 @@ import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class SemaphoreDemo {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
+        executeSequentially();
+    }
+
+    /**
+     * 交替执行
+     *
+     * @throws InterruptedException
+     */
+    public static void alternateExecution() throws InterruptedException {
+        Semaphore s1 = new Semaphore(1);
+        Semaphore s2 = new Semaphore(1);
+        Semaphore s3 = new Semaphore(1);
+        s2.acquire();
+        s3.acquire();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    s1.acquire();
+                    System.out.println("A");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    s2.release();
+                }
+            }
+        }, "A").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    s2.acquire();
+                    System.out.println("B");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    s3.release();
+                }
+            }
+        }, "B").start();
+        new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    s3.acquire();
+                    System.out.println("C");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    s1.release();
+                }
+            }
+        }, "C").start();
+    }
+
+    /**
+     * 依次执行
+     */
+    public static void executeSequentially() throws InterruptedException {
+        Semaphore s1 = new Semaphore(1);
+        Semaphore s2 = new Semaphore(1);
+        Semaphore s3 = new Semaphore(1);
+        s2.acquire();
+        s3.acquire();
+        new Thread(() -> {
+                try {
+                    s1.acquire();
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("A");
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    s2.release();
+                }
+        }, "A").start();
+        new Thread(() -> {
+                try {
+                    s2.acquire();
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("B");
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    s3.release();
+                }
+        }, "B").start();
+        new Thread(() -> {
+            try {
+                s3.acquire();
+                for (int i = 0; i < 10; i++) {
+                    System.out.println("C");
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                s1.release();
+            }
+        }, "C").start();
+    }
+
+    public static void test() {
         Semaphore semaphore = new Semaphore(3);//模拟三个停车位
         for (int i = 1; i <= 6; i++) {//模拟6部汽车
             new Thread(() -> {
